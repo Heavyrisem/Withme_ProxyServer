198f95a69c48b43d2abf09f473dc285e310baa7c
heavyrisem <pyo1748@gmail.com>
2021-09-02 19:41:10 +0900

    Payload fix

--- middleware/index.ts
+++ middleware/index.ts
@@ -3,7 +3,7 @@ import { IncomingMessage, ServerResponse } from 'http';
 
 export default {
     Parser: (req: IncomingMessage, res: ServerResponse, next: (err?: any) => void) => {
-        express.json()(req, res, (err) => {
+        express.json({limit: "50mb"})(req, res, (err) => {
             if (err) next(err);
             else express.urlencoded({extended: true})(req, res, next);
         });
