c5191e955e38597c4d6f826ab7f630888192fef2
heavyrisem <pyo1748@gmail.com>
2021-09-02 18:25:27 +0900

    v2

--- main.ts
+++ main.ts
@@ -1,25 +1,16 @@
 import express from 'express';
 import http from 'http';
-import socketio from 'socket.io'
 
 import middleware from './middleware';
-
 import Prediction from './routes/Prediction';
-import socket from './routes/socket';
-
-import './model/DB';
 
 const App = express();
 const Server = http.createServer(App);
-const io = new socketio.Server(Server, {transports: ['websocket', 'polling'], maxHttpBufferSize: 100000000000});
 
 App.use(middleware.Parser);
-App.use(middleware.NUGU_Dev);
 
 App.use('/', Prediction);
 
-io.on('connection', socket);
-
 Server.listen(3000, () => {
-    console.log("Withme Server online");
+    console.log("Withme Proxy V2 Server online");
 })
--- routes/Prediction/index.ts
+++ routes/Prediction/index.ts
@@ -1,52 +1,42 @@
 import { Request, Response, Router } from "express";
-import global from "../../global";
 import middleware from "../../middleware";
-import DeviceDB from "../../model/DeviceDB";
 import Prediction from "../../model/Prediction";
-import { NUGU_Request, NUGU_Response, Socket_Data_T } from "../Types";
+import * as googleTTS from 'google-tts-api';
 
 
 const router = Router();
 
-const Predict = async (req: Request<any,any,NUGU_Request>, res: Response) => {
-    let nuguResponse = new NUGU_Response<{result: string}>({result: "미인증된 캔들입니다. 인증을 먼저 진행해 주세요"});
+const Predict = async (req: Request<any,any,{imageData: string}>, res: Response) => {
+    let textResult = "";
+    if (req.body.imageData) {
+        console.log("base64 length", req.body.imageData.length);
 
-    if (req.body.profile) {
         try {
-            const mobileID = await DeviceDB.FindDevice(req.body.profile.privatePlay.deviceUniqueId);
-            console.log(Object.keys(global.SOCKET_CLIENTS));
-            if (global.SOCKET_CLIENTS[mobileID]) {
-                // Send event to mobile
-                // console.log("Emit event");
-                global.SOCKET_CLIENTS[mobileID].once("ImageCapture", async (data: Socket_Data_T) => {
-                    console.log("base64 length", data.imageData.length);
-
-                    try {
-                        let result;
-                        switch (req.url) {
-                            case "/caption": result = await Prediction.Caption(Buffer.from(data.imageData, "base64")); break;
-                            case "/ocr": result = await Prediction.OCR(Buffer.from(data.imageData, "base64")); break;
-                            default: result = `${req.url} 은 잘못된 경로입니다.`;
-                        }
-                        nuguResponse.output.result = await Prediction.Translate_ENtoKO(result);
-                    } catch (err) {
-                        console.log(err);
-                        nuguResponse.output.result = `오류 ${err}}, 인공지능 서버에서 오류가 발생했습니다.`;
-                    } finally {return res.send(nuguResponse.toString())}
-                });
-
-                global.SOCKET_CLIENTS[mobileID].emit("ImageCapture", true);
-            } else {
-                // mobile device is offline
-                // console.log("Target Device is offline");
-                nuguResponse.output.result = "모바일 디바이스가 오프라인입니다.";
-                return res.send(nuguResponse.toString());
+            let result;
+            switch (req.url) {
+                case "/caption": result = await Prediction.Caption(Buffer.from(req.body.imageData, "base64")); break;
+                case "/ocr": result = await Prediction.OCR(Buffer.from(req.body.imageData, "base64")); break;
+                default: textResult = `${req.url} 은 잘못된 경로입니다.`;
             }
+            (result)&& (textResult = await Prediction.Translate_ENtoKO(result))
         } catch (err) {
-            nuguResponse.output.result = "미인증된 캔들입니다. 인증을 먼저 진행해 주세요";
-            return res.send(nuguResponse.toString());
+            console.log(err);
+            textResult = `오류 ${err}}, 인공지능 서버에서 오류가 발생했습니다.`;
         }
-    } else return res.send(nuguResponse.toString());
+    } else return textResult = "이미지 데이터가 없습니다.";
+
+    try {
+        const base64result = await googleTTS.getAudioBase64(textResult, {
+            lang: 'ko',
+            slow: false,
+            host: 'https://translate.google.com',
+            timeout: 10000,
+        });
+        res.send({result: base64result});
+    } catch (err) {
+        console.log(err);
+        res.send({err: err});
+    }
 }
 
 router.post('/caption', middleware.Parser, Predict);
