c4153463a03627878cebc93e0258ff8727e1108a
heavyrisem <pyo1748@gmail.com>
2021-09-06 13:52:16 +0900

    timeout handler fix

--- routes/Prediction/index.ts
+++ routes/Prediction/index.ts
@@ -21,38 +21,38 @@ const Predict = async (req: Request<any,any,NUGU_Request>, res: Response) => {
             
             for (const ID of mobileIDlist)
                 if (global.SOCKET_CLIENTS[ID]) {onlineDeviceNum++;onlineDeviceID=ID}
-                
-                if (onlineDeviceID && onlineDeviceNum == 1) {
-                    // Send event to mobile
-                    // console.log("Emit event");
-                    const Timeout = setTimeout(() => {
-                        global.SOCKET_CLIENTS[onlineDeviceID].off("ImageCapture", ImageHandler);
-                        nuguResponse.output.result = "요청을 처리하는데 너무 오래 걸립니다.";
-                        return res.send(nuguResponse.toString());
-                    }, 8000);
-                    
-                    global.SOCKET_CLIENTS[onlineDeviceID].once("ImageCapture", ImageHandler);
-                    global.SOCKET_CLIENTS[onlineDeviceID].emit("ImageCapture", true);
 
-                    async function ImageHandler (data: Socket_Data_T) {
-                        clearTimeout(Timeout);
-                        console.log("base64 length", data.imageData.length);
+            if (onlineDeviceID && onlineDeviceNum == 1) {
+                const ImageHandler = async (data: Socket_Data_T) => {
+                    clearTimeout(Timeout);
+                    console.log("base64 length", data.imageData.length);
 
-                        try {
-                            let result;
-                            switch (req.url) {
-                                case "/caption": 
-                                    result = await Prediction.Caption(Buffer.from(data.imageData, "base64"));
-                                    result = await Prediction.Translate_ENtoKO(result); break;
-                                case "/ocr": result = await Prediction.OCR(Buffer.from(data.imageData, "base64")); break;
-                                default: result = `${req.url} 은 잘못된 경로입니다.`;
-                            }
-                            nuguResponse.output.result = result;
-                        } catch (err) {
-                            if (err == DefaultError.TIMEOUT) nuguResponse.output.result = '인공지능 서버 요청 시간이 초과되었습니다.';
-                            else nuguResponse.output.result = `오류 ${err}, 인공지능 서버에서 오류가 발생했습니다.`;
-                        } finally {return res.send(nuguResponse.toString())}
-                    }
+                    try {
+                        let result;
+                        switch (req.url) {
+                            case "/caption": 
+                                result = await Prediction.Caption(Buffer.from(data.imageData, "base64"));
+                                result = await Prediction.Translate_ENtoKO(result); break;
+                            case "/ocr": result = await Prediction.OCR(Buffer.from(data.imageData, "base64")); break;
+                            default: result = `${req.url} 은 잘못된 경로입니다.`;
+                        }
+                        nuguResponse.output.result = result;
+                    } catch (err) {
+                        if (err == DefaultError.TIMEOUT) nuguResponse.output.result = '인공지능 서버 요청 시간이 초과되었습니다.';
+                        else nuguResponse.output.result = `오류 ${err}, 인공지능 서버에서 오류가 발생했습니다.`;
+                    } finally {return res.send(nuguResponse.toString())}
+                }
+                // Send event to mobile
+                // console.log("Emit event");
+                const Timeout = setTimeout(() => {
+                    global.SOCKET_CLIENTS[onlineDeviceID].removeListener("ImageCapture", ImageHandler);
+                    nuguResponse.output.result = "요청을 처리하는데 너무 오래 걸립니다.";
+                    return res.send(nuguResponse.toString());
+                }, 3000);
+
+                global.SOCKET_CLIENTS[onlineDeviceID].once("ImageCapture", ImageHandler);
+
+                global.SOCKET_CLIENTS[onlineDeviceID].emit("ImageCapture", true);
             } else if (onlineDeviceNum > 1) {
                 nuguResponse.output.result = "연결된 모바일 디바이스가 너무 많습니다. 한 개만 연결해 주세요.";
                 return res.send(nuguResponse.toString());
--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ axios.default({
     method: "POST",
     url: "https://withme.heavyrisem.xyz/caption",
     data: {
-        profile: {privatePlay:{deviceUniqueId: "TestID"}}
+        profile: {privatePlay:{deviceUniqueId: "NU110_0A8A36"}}
     }
 }).then(value => {
     console.log(value.data);
